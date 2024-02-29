2af8d0c7ee02067722ef5152bce2c0012b495362
Abraham Osco <aosco@fi.uba.ar>
2024-02-29 20:23:28 -0300

    Subo HachaTest inical para el TP1

---
+++ TP1-Test/HachaTest.java
@@ -0,0 +1,43 @@
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HachaTest {
+    /*
+    @Test
+    public void testHachaSiSeUsa5VecesDeberiaLanzarExcepcionAlUsarlaConDurabilidaNegativa(){
+        //Arrange
+        Hacha unaHacha = new Hacha();
+
+        //Act
+        unaHacha.usar(); // durabilidad 9
+        unaHacha.usar(); // durabilidad 7
+        unaHacha.usar(); // durabilidad 4
+        unaHacha.usar(); // durabilidad 0
+
+        // Assert
+        assertThrows(NoSePuedeUsarLaHerramientaDurabilidadNegativaError.class, () -> {
+            unaHacha.usar(); // durabilidad es cero si usamos la herramienta lanzamos la excpecion de arriba
+        } );
+    }
+
+    @Test
+    public void testHachaSiUsamos3VecesYLuegoUnaCuartaVezNoDeberiaLanzarExcpecion(){
+        //Arrange
+        Hacha unaHacha = new Hacha();
+
+        //Act
+        unaHacha.usar(); // durabilidad 9
+        unaHacha.usar(); // durabilidad 7
+        unaHacha.usar(); // durabilidad 4
+
+        // Assert
+        assertDoesNotThrow( () -> {
+            unaHacha.usar(); //durabilidad 0 todo ok
+        });
+    }
+    */
+
+}
