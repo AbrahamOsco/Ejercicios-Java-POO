58565db207ec71f791ac0b58be56ba144f36beab
Abraham Osco <aosco@fi.uba.ar>
2024-03-02 14:24:56 -0300

    Se actualiza la consigna del TP2 se agregan mas test

--- TP2-Test/AlgoFixTest.java
+++ TP2-Test/AlgoFixTest.java
@@ -1,14 +1,18 @@
 package TestTP2;
 
+import Resueltos.TP2AlgoFix.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class AlgoFixTest {
+
     @Test
     public void testSeDebeObtenerElMejorPresupuestoParaPintarConPincel(){
         //Arrange
         AlgoFix algoFix = new AlgoFix();
+
         // Con la herramienta pincel se tarda 3 horas en pintar un metro cuadrado
         // y cada metro cuadrado tiene 4 libros de pintura
 
@@ -23,13 +27,13 @@ public class AlgoFixTest {
         // Colocamos el nombre del pintor que usa Rodillo y el precio que cobra por hora.
 
 
-        var unaPintura = algoFix.crearPintura("Azul", 200); // creamos una pintura de color Azul que cuesta 200 el litro.
-        var presupuesto = algoFix.presupuestoMasBaratoConPincel(5, unaPintura); // 5 metros cuadrados y usando la pintura azul.
+        Pintura unaPintura = algoFix.crearPintura("Azul", 200); // creamos una pintura de color Azul que cuesta 200 el litro.
+        Pintor presupuesto = algoFix.presupuestoMasBaratoConPincel(5, unaPintura); // 5 metros cuadrados y usando la pintura azul.
         // es evidente que tanto unaPintura y presupuesto deben ser objetos claramente y deben eliminar el "var"
 
         // 5 metros cuadrado x 3 hora/metroCuadrado * 500 pesos/hora + 5 metrosCuadrados * 4 LitroPintura/metroCuadrado * 200 Pesos/LitroPintura
         // hacemos el calculo del precio y ademas tenemos que el que cobra mas barato es Ricardo Yoku  es evidente tiene menor precio hora.
-        int precioEsperado = (5 * 3 * 500) + (5*4*200);
+        double precioEsperado = (5 * 3 * 500) + (5*4*200);
         String nombreEsperado  = "Ricardo Yoku";
 
         //Assert
@@ -61,7 +65,7 @@ public class AlgoFixTest {
 
         // 7 metros cuadrado x 2 hora/metroCuadrado * 700 pesos/hora + 7 metrosCuadrados * 6 LitroPintura/metroCuadrado * 400 Pesos/LitroPintura
         // hacemos el calculo del precio y ademas tenemos que el que cobra mas barato es Sandy Mendez  es evidente tiene menor precio hora.
-        int precioEsperado = (7 * 2 * 700) + (7 * 6 * 400);
+        double precioEsperado = (7 * 2 * 700) + (7 * 6 * 400);
         String nombreEsperado  = "Sandy Mendez";
 
         //Assert
@@ -95,8 +99,107 @@ public class AlgoFixTest {
         // hacemos el calculo del precio y ademas tenemos que el que cobra mas barato es Sandy Mendez  es evidente tiene menor precio hora.
 
         // aca tenemos el empate ambos cobran lo mismo, devolvemos el primer minimo que encontramos que es de Marco Tantrum
-        int precioEsperado = (4 * 2 * 1000) + (4 * 6 * 700);
+        double precioEsperado = (4 * 2 * 1000) + (4 * 6 * 700);
         String nombreEsperado  = "Marco Tantrum";
+
+        //Assert
+        assertEquals(precioEsperado, presupuesto.getValor());
+        assertEquals(nombreEsperado, presupuesto.getResponsable());
+    }
+
+    @Test
+    public void testSePideCalculaElMejorPresupuestoParaPintarConPincelPeroNoHayPintoresConPincelDeberiaLanzarExcepcion(){
+        //Arrange
+        AlgoFix algoFix = new AlgoFix();
+
+        //Act
+        algoFix.registrarPintorDeRodillo("Nancy Prieto", 100);
+
+        var unaPintura = algoFix.crearPintura("Marron", 900); // creamos una pintura de color Azul que cuesta 200 el litro.
+
+        assertThrows( NoExistenPintoresConPincelError.class, () -> {
+            algoFix.presupuestoMasBaratoConPincel(5, unaPintura);
+        } );
+    }
+
+    @Test
+    public void testSePideCalculaElMejorPresupuestoParaPintarConRodilloPeroNoHayPintoresConRodilloDeberiaLanzarExcepcion(){
+        //Arrange
+        AlgoFix algoFix = new AlgoFix();
+
+        //Act
+        algoFix.registrarPintorDePincel("Anthony Perez", 900);
+
+        var unaPintura = algoFix.crearPintura("Marron", 900); // creamos una pintura de color Azul que cuesta 200 el litro.
+
+        assertThrows( NoExistenPintoresConRodilloError.class, () -> {
+            algoFix.presupuestoMasBaratoConRodillo(5, unaPintura);
+        } );
+    }
+
+    @Test
+    public void testSeDebeObtenerElMejorPresupuestoParaPintarConRodilloDescuentoDel50PorcientoAlPintarMasOIgualQue100MetrosCuadrados(){
+        //Arrange
+        AlgoFix algoFix = new AlgoFix();
+        // Con la herrmienta Rodillo se tarda 2 horas en pintar un metro cuadrado
+        // y cada metro cuadrado tiene 6 libros de pintura
+
+        //Act
+        algoFix.registrarPintorDePincel("Angie Martinez", 100);
+        // Colocamos el nombre del pintor que usa Pincel y el precio que cobra por hora.
+
+        algoFix.registrarPintorDeRodillo("Sandy Mendez", 700);
+        // Colocamos el nombre del pintor que usa Pincel y el precio que cobra por hora.
+
+        algoFix.registrarPintorDeRodillo("Ross Contreras", 1000);
+        // Colocamos el nombre del pintor que usa Rodillo y el precio que cobra por hora.
+
+        var unaPintura = algoFix.crearPintura("Blanca", 400); // creamos una pintura de color Azul que cuesta 200 el litro.
+        var presupuesto = algoFix.presupuestoMasBaratoConRodillo(100, unaPintura); // 5 metros cuadrados y usando la pintura azul.
+        // es evidente que tanto unaPintura y presupuesto deben ser objetos claramente y deben eliminar el "var"
+
+        // 7 metros cuadrado x 2 hora/metroCuadrado * 700 pesos/hora + 7 metrosCuadrados * 6 LitroPintura/metroCuadrado * 400 Pesos/LitroPintura
+        // hacemos el calculo del precio y ademas tenemos que el que cobra mas barato es Sandy Mendez  es evidente tiene menor precio hora.
+
+        // Si la cantidad de metros cuadrados es >= 100 m2 entonces hacemos el descuento del 50% al presupuesto final.
+        double precioEsperado = 0.5*( (100 * 2 * 700) + (100 * 6 * 400) );
+        String nombreEsperado  = "Sandy Mendez";
+
+        //Assert
+        assertEquals(precioEsperado, presupuesto.getValor());
+        assertEquals(nombreEsperado, presupuesto.getResponsable());
+    }
+
+
+    @Test
+    public void testSeDebeObtenerElMejorPresupuestoParaPintarConPincelDescuentoDel50PorcientoPorPintarMasOIgualQue100MetrosCuadrados(){
+        //Arrange
+        AlgoFix algoFix = new AlgoFix();
+
+        // Con la herramienta pincel se tarda 3 horas en pintar un metro cuadrado
+        // y cada metro cuadrado tiene 4 libros de pintura
+
+        //Act
+        algoFix.registrarPintorDePincel("Ricardo Yoku", 500);
+        // Colocamos el nombre del pintor que usa Pincel y el precio que cobra por hora.
+
+        algoFix.registrarPintorDePincel("Ana Sandy", 800);
+        // Colocamos el nombre del pintor que usa Pincel y el precio que cobra por hora.
+
+        algoFix.registrarPintorDeRodillo("Kelvin Contreras", 600);
+        // Colocamos el nombre del pintor que usa Rodillo y el precio que cobra por hora.
+
+
+        Pintura unaPintura = algoFix.crearPintura("Azul", 200); // creamos una pintura de color Azul que cuesta 200 el litro.
+        Pintor presupuesto = algoFix.presupuestoMasBaratoConPincel(105, unaPintura); // 5 metros cuadrados y usando la pintura azul.
+        // es evidente que tanto unaPintura y presupuesto deben ser objetos claramente y deben eliminar el "var"
+
+        // 5 metros cuadrado x 3 hora/metroCuadrado * 500 pesos/hora + 5 metrosCuadrados * 4 LitroPintura/metroCuadrado * 200 Pesos/LitroPintura
+        // hacemos el calculo del precio y ademas tenemos que el que cobra mas barato es Ricardo Yoku  es evidente tiene menor precio hora.
+
+        // Si la cantidad de metros cuadrados es >= 100 m2 entonces hacemos el descuento del 50% al presupuesto final.
+        double precioEsperado = 0.5*((105 * 3 * 500) + (105*4*200));
+        String nombreEsperado  = "Ricardo Yoku";
 
         //Assert
         assertEquals(precioEsperado, presupuesto.getValor());
